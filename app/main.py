84a628ceb058f51fb16ffcb30715b04778101a8a
agholizadgha <agholizadgha@mun.ca>
2024-04-15 12:11:36 -0230

    revert app.main to original version

--- app/main.py
+++ app/main.py
@@ -1,28 +1,28 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-# from . import models
-# from .config import settings
-# from .database import engine
-# from .routers import auth, posts, users
+from . import models
+from .config import settings
+from .database import engine
+from .routers import auth, posts, users
 
-# models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 origins = ["*"]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# app.include_router(posts.router)
-# app.include_router(users.router)
-# app.include_router(auth.router)
+app.include_router(posts.router)
+app.include_router(users.router)
+app.include_router(auth.router)
 
 @app.get("/")
 def root():
